02e50785c9f9fa4e48c3682d7a59b48c55b93468
alin-corodescu <alin.corodescu@gmail.com>
2017-10-11 21:37:38 +0300

    built basic chatbot

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import aiml
+import fileinput
+
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("LOAD AIML B")
+
+while True:
+    print(kernel.respond(input()))
