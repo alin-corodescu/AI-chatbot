8fc8382d7f0c3280cd6120af1fb8912c5c6b1787
alin-corodescu <alin.corodescu@gmail.com>
2017-10-12 23:05:34 +0300

    working setting up a new person

---
+++ introduction.py
@@ -0,0 +1,103 @@
+import aiml
+
+sessionId = 123
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("load aiml b",sessionId)
+kernel.verbose(True)
+# TODO : populate this list with dictionaries (nume,age,job)
+persons = []
+
+
+def can_identify(nume, age, occupation):
+    # check if the nume appears twice
+    # fac un for care sa imi aduca persoanele
+    nume_filtered = filter(lambda x : x["nume"] == nume, persons)
+    if len(list(nume_filtered)) == 1:
+        return True
+    if age != None:
+        age_filtered = filter(lambda x : x["age"] == age, nume_filtered)
+        if len(list(age_filtered)) == 1:
+            return True
+        if occupation != None:
+            job_filtered = filter(lambda x : x["job"] == occupation, age_filtered)
+            if len(list(job_filtered)) == 1:
+                return True
+
+    return False
+
+
+def find_age():
+    kernel.setPredicate("age","",sessionId)
+    kernel.setPredicate("topic","ASKAGE",sessionId)
+    print(kernel.respond("secret trigger",sessionId))
+    while kernel.getPredicate("age",sessionId) == "":
+        print(kernel.respond(input("age>>"),sessionId))
+    return kernel.getPredicate("age",sessionId)
+
+
+def find_occupation():
+    kernel.setPredicate("job","",sessionId)
+    kernel.setPredicate("topic","ASKJOB",sessionId)
+    print(kernel.respond("secret trigger",sessionId))
+    while kernel.getPredicate("job",sessionId) == "":
+        print(kernel.respond(input("job>>"),sessionId))
+    return kernel.getPredicate("job",sessionId)
+
+# TODO implement storing method
+# If there is no entry with this person
+def add_person(nume, age, occupation):
+    pass
+
+
+# Get confirmation that all the entries are correct
+def confirm():
+    kernel.setPredicate("topic","CONFIRM",sessionId)
+    print(kernel.respond("secret trigger",sessionId))
+    while kernel.getPredicate("conf",sessionId) == "":
+        print(kernel.respond(input("confirm>>"),sessionId))
+    if kernel.getPredicate("conf",sessionId) == "TRUE":
+        return True
+    return False
+
+
+while len(kernel.getPredicate("nume",sessionId)) == 0:
+    print(kernel.getPredicate("nume",sessionId))
+    print(kernel.respond(input("nume>>"),sessionId))
+
+nume = kernel.getPredicate("nume",sessionId)
+age = None
+occupation = None
+
+while not can_identify(nume, age, occupation):
+    if age == None:
+        age = find_age()
+    elif occupation == None:
+        occupation = find_occupation()
+    else:
+        break
+
+
+def get_matched_age(nume):
+    for person in persons:
+        if person["nume"] == nume:
+            return person["age"]
+
+def get_matched_occupation(nume, age):
+    for person in persons:
+        if person["nume"] == nume and person["age"] == age:
+            return person["occupation"]
+
+if age == None:
+    age = get_matched_age(nume)
+
+if occupation == None:
+    occupation = get_matched_occupation(nume,age)
+
+if not confirm():
+    age = find_age()
+    occupation = find_occupation()
+# New person around
+if not can_identify(nume, age, occupation):
+    add_person(nume,age,occupation)
+
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import fileinput
 
 kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
-kernel.respond("LOAD AIML B")
 
 while True:
+    kernel.respond("load aiml b",123)
+    sessionData = kernel.getSessionData(123);
+    print(kernel.getPredicate("topic", 123))
+    print(sessionData)
+    print(kernel.getBotPredicate("topic"))
     print(kernel.respond(input()))
